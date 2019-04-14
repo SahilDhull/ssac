1025ec3a283726243a8b30495d425f0fee73b691
shubhuiitk <shubhuiitk@gmail.com>
2019-04-14 23:13:51 +0530

    dasdada

--- src/test.go
+++ src/test.go
@@ -1,21 +1,24 @@
 package main;
 
-func ackermann(m int, n int) int {
-	if m == 0 {
-		return n + 1;
-	};
+// func ackermann(m int, n int) int {
+// 	if m == 0 {
+// 		return n + 1;
+// 	};
 
-	if n == 0 {
-		return ackermann(m-1, 1);
-	};
+// 	if n == 0 {
+// 		return ackermann(m-1, 1);
+// 	};
 
-	return ackermann(m-1, ackermann(m, n-1));
+// 	return ackermann(m-1, ackermann(m, n-1));
+// };
 
-	// print m;
-	// print n;
-	// return 1;
-};
+// func main() {
+// 	print ackermann(3, 2);
+// };
 
-func main() {
-	print ackermann(3, 2);
+func main(){
+	// var a int = 3;
+	var b int = -2;
+	// print a;
+	print b;
 };
