2c76cc3adf8199656cdf7f5cbf6785bb3caf8c36
ankitkr1606 <ankitkr1606@gmail.com>
2019-04-14 21:43:40 +0530

    pointer

--- src/test.go
+++ src/test.go
@@ -1,45 +1,9 @@
 package main;
 
-<<<<<<< HEAD
 func main(){
-	var k string;
-	var p *string;
-	p = &k;
-	*p = "ok";
-	// *p = 3;
-	// print *p;
-	// print "\n";
+	var p *int;
+	var a int = 5;
+	p = &a;
+	*p = 1;
+	print *p;
 };
-=======
-type side struct{
-	a int;
-	b string;
-};
-
-type rect struct {
-    name string;
-    age  int;
-    part type side;
-};
-
-func f(a type rect) type rect {
-	a.name = "\nCOOL\n";
-	a.part.b = "\ngggggggg\n";
-	return a;
-};
-
-func main(){
-	var str string = "\n";
-	var b type rect;
-	b.age = 2;
-	b.name = "hey\n";
-	b.part.a = 1;
-	b.part.b = "Nope\n";
-	print b.part.b;
-	print b.name;
-	print b.age;
-	b = f(b);
-	print b.name;
-	print b.part.b;
-};
->>>>>>> 63321e01c6978282ce8a1337319c1977b366b230
